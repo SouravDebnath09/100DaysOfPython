4b32670f12a958c6cd0e263572834accf46eda6b
Sourav Debnath <pickki.deb@gmail.com>
2018-11-01 00:13:17 +0530

    first file change

--- 31-11-2018/ArraySum.py
+++ 31-11-2018/ArraySum.py
@@ -2,5 +2,5 @@
 sample =[12,33,54,89,776,3]
 # Don't need to loop through, sum function will do it for you
 total = sum(sample)
-# printing the total value
+# printing the total valu e
 print(total)
